dec89747d9869185d01ce5d9b9049b213fb730dd
Sean McClure <seanaaron100@gmail.com>
2025-03-04 01:43:41 +0000

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+const main = {
+    events : function() {
+        document.getElementById("sign_in_button").addEventListener("click", function() {
+            alert("hello")
+        });
+    }
+};
+
