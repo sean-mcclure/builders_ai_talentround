85145f446df9c4d54de08b61cc0d461db50fb7ac
Sean McClure <seanaaron100@gmail.com>
2025-05-07 20:18:34 +0000

    lesson 5 - creating a dynamic side menu

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    side_menu_options : {
+        apply : ["fill out details", "upload resume", "upload cover letter"],
+        screening : [],
+        shortlisting : []
+    }
+};
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,32 @@
 const main = {
-    events : function() {
-        document.getElementById("sign_in_button").addEventListener("click", function() {
-            alert("hello")
-        });
+    side_menu : {
+        attach_events : function() {
+            document.querySelectorAll(".top_banner_buttons").forEach(function(elem) {
+                elem.addEventListener("click", function() {
+                    main.side_menu.open_menu({
+                        menu_items : config.side_menu_options[elem.innerText.toLowerCase().trim()]
+                    });
+                });
+            });
+            document.getElementById("close_side_menu").addEventListener("click", function() {
+                main.side_menu.close_menu();
+            });
+        },
+        open_menu : function(options) {
+            options.menu_items.forEach(function(elem, i) {
+                const item = document.createElement("div");
+                item.innerText = elem;
+                item.classList.add("side_menu_items");
+                document.getElementById("side_menu").append(item);
+                if(i === 0) {
+                    item.style.marginTop = "50px";
+                };
+            });
+            document.getElementById("side_menu").classList.add("open");
+        },
+        close_menu : function() {
+            document.getElementById("side_menu").classList.remove("open");
+        }
     }
 };
 
